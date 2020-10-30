29270753d13de7d25ffb877cf0a44531259d0bc9
saki yamamoto <saki.yamamoto@d2cdot.co.jp>
2020-10-30 15:35:45 +0900

    アニメーション追加

--- CardApp/ContentView.swift
+++ CardApp/ContentView.swift
@@ -16,19 +16,35 @@ struct ContentView: View {
     @State var japanese = "かばん"
     @State var isJapanese = true
     var body: some View {
-        VStack {
-            Text(isJapanese ? japanese : cards[japanese]!)
-            HStack {
-                Button(action: {self.isJapanese.toggle()}) {
-                    Text(/*@START_MENU_TOKEN@*/"裏返す"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: 0.97, green: 0.96, blue: 0.56)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ZStack {
+                    Image("card")
+                        .resizable()
+                        .frame(width: 300.0, height: 133.0)
+                        .shadow(radius: 3)
+                        .rotation3DEffect(.degrees(isJapanese ? 0:180), axis: (x:0,y:1,z:0))
+                        .animation(.spring())
+                        .onTapGesture {
+                            self.isJapanese.toggle()
+                    }
+                    Text(isJapanese ? japanese : cards[japanese]!)
                 }
-                Button(action: {self.isJapanese = true
-                    self.japanese = self.cards.randomElement()!.key
-                }) {
-                    Text(/*@START_MENU_TOKEN@*/"次へ"/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Button(action: {self.isJapanese.toggle()}) {
+                        Text(/*@START_MENU_TOKEN@*/"裏返す"/*@END_MENU_TOKEN@*/)
+                    }
+                    Button(action: {self.isJapanese = true
+                        self.japanese = self.cards.randomElement()!.key
+                    }) {
+                        Text(/*@START_MENU_TOKEN@*/"次へ"/*@END_MENU_TOKEN@*/)
+                    }
                 }
             }
         }
+        
     }
 }
 
